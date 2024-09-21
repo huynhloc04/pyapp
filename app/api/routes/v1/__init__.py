726389bc06aa8a25a3b836f38590d04003fe2f31
huynhloc04 <hloc5874@gmail.com>
2024-09-21 19:01:28 +0700

    feat(auth): add google-sso

--- app/api/routes/v1/__init__.py
+++ app/api/routes/v1/__init__.py
@@ -3,7 +3,7 @@ from importlib import import_module
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/v1")
-routes = ("user", "group", "product", "order", "store")
+routes = ("google_sso", "user", "group", "product", "order", "store")
 for module_name in routes:
     api_module = import_module(f"app.api.routes.v1.{module_name}")
     api_module_router = api_module.router
